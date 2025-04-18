61e9d9f28efb6210ec14b44b4c5401a76b079985
mubiii <mubeensaeed@07gmail.com>
2025-04-18 22:00:24 +0500

    2nd commit

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -31,7 +31,7 @@ class InstallCommand extends Command
             );
             
             // Check if the selected provider is installed
-            $this->checkAndProvideDependencyInstructions($authProvider);
+            $this->installDependencyIfNeeded($authProvider);
         }
         
         // Create config file
@@ -39,10 +39,15 @@ class InstallCommand extends Command
         
         // Publish views and migrations
         $this->info('Publishing resources...');
-        $this->callSilently('vendor:publish', [
+        $this->call('vendor:publish', [
             '--provider' => 'Mubeen\LaravelUserCrud\LaravelUserCrudServiceProvider',
             '--tag' => 'laravel-user-crud'
         ]);
+        
+        if ($interfaceType === 'api' && $authProvider === 'passport' && class_exists('\\Laravel\\Passport\\Passport')) {
+            $this->info('Installing Laravel Passport keys...');
+            $this->call('passport:install');
+        }
         
         $this->info('Laravel User CRUD package has been installed successfully!');
         
@@ -50,59 +55,102 @@ class InstallCommand extends Command
         $this->outputNextSteps($interfaceType, $authProvider);
     }
     
-    private function checkAndProvideDependencyInstructions($authProvider)
+    private function installDependencyIfNeeded($authProvider)
     {
         switch ($authProvider) {
             case 'sanctum':
                 if (!class_exists(\Laravel\Sanctum\Sanctum::class)) {
-                    $this->warn('Laravel Sanctum is not installed.');
-                    $this->newLine();
-                    $this->info('Please run the following commands:');
-                    $this->info('1. composer require laravel/sanctum');
-                    $this->info('2. php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"');
-                    $this->info('3. php artisan migrate');
-                    $this->newLine();
-                    
-                    if (!$this->confirm('Do you want to continue with the setup? You can complete these steps later.', true)) {
-                        $this->info('Installation aborted. Please run the required commands and try again.');
-                        exit(1);
+                    if ($this->confirm('Laravel Sanctum is not installed. Would you like to install it now?', true)) {
+                        $this->info('Installing Laravel Sanctum...');
+                        if ($this->runCommand('composer require laravel/sanctum')) {
+                            $this->call('vendor:publish', [
+                                '--provider' => 'Laravel\Sanctum\SanctumServiceProvider'
+                            ]);
+                            $this->call('migrate');
+                            $this->info('Laravel Sanctum installed successfully.');
+                        } else {
+                            $this->error('Failed to install Laravel Sanctum.');
+                            $this->info('Please run these commands manually:');
+                            $this->info('1. composer require laravel/sanctum');
+                            $this->info('2. php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"');
+                            $this->info('3. php artisan migrate');
+                            
+                            if (!$this->confirm('Do you want to continue with the setup?', false)) {
+                                $this->info('Installation aborted.');
+                                exit(1);
+                            }
+                        }
                     }
                 }
                 break;
                 
             case 'passport':
                 if (!class_exists(\Laravel\Passport\Passport::class)) {
-                    $this->warn('Laravel Passport is not installed.');
-                    $this->newLine();
-                    $this->info('Please run the following commands:');
-                    $this->info('1. composer require laravel/passport');
-                    $this->info('2. php artisan passport:install');
-                    $this->newLine();
-                    
-                    if (!$this->confirm('Do you want to continue with the setup? You can complete these steps later.', true)) {
-                        $this->info('Installation aborted. Please run the required commands and try again.');
-                        exit(1);
+                    if ($this->confirm('Laravel Passport is not installed. Would you like to install it now?', true)) {
+                        $this->info('Installing Laravel Passport...');
+                        if ($this->runCommand('composer require laravel/passport')) {
+                            $this->info('Laravel Passport installed successfully.');
+                        } else {
+                            $this->error('Failed to install Laravel Passport.');
+                            $this->info('Please run this command manually: composer require laravel/passport');
+                            
+                            if (!$this->confirm('Do you want to continue with the setup?', false)) {
+                                $this->info('Installation aborted.');
+                                exit(1);
+                            }
+                        }
                     }
                 }
                 break;
                 
             case 'jwt':
                 if (!class_exists(\Tymon\JWTAuth\JWTAuth::class)) {
-                    $this->warn('JWT Auth is not installed.');
-                    $this->newLine();
-                    $this->info('Please run the following commands:');
-                    $this->info('1. composer require tymon/jwt-auth');
-                    $this->info('2. php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"');
-                    $this->info('3. php artisan jwt:secret');
-                    $this->newLine();
-                    
-                    if (!$this->confirm('Do you want to continue with the setup? You can complete these steps later.', true)) {
-                        $this->info('Installation aborted. Please run the required commands and try again.');
-                        exit(1);
+                    if ($this->confirm('JWT Auth is not installed. Would you like to install it now?', true)) {
+                        $this->info('Installing JWT Auth...');
+                        if ($this->runCommand('composer require tymon/jwt-auth')) {
+                            $this->call('vendor:publish', [
+                                '--provider' => 'Tymon\JWTAuth\Providers\LaravelServiceProvider'
+                            ]);
+                            $this->call('jwt:secret');
+                            $this->info('JWT Auth installed successfully.');
+                        } else {
+                            $this->error('Failed to install JWT Auth.');
+                            $this->info('Please run these commands manually:');
+                            $this->info('1. composer require tymon/jwt-auth');
+                            $this->info('2. php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"');
+                            $this->info('3. php artisan jwt:secret');
+                            
+                            if (!$this->confirm('Do you want to continue with the setup?', false)) {
+                                $this->info('Installation aborted.');
+                                exit(1);
+                            }
+                        }
                     }
                 }
                 break;
         }
+    }
+    
+    /**
+     * Run a shell command.
+     * 
+     * @param string $command
+     * @return bool
+     */
+    private function runCommand($command)
+    {
+        $this->info("Running: $command");
+        
+        $output = null;
+        $returnCode = null;
+        
+        exec($command . ' 2>&1', $output, $returnCode);
+        
+        foreach ($output as $line) {
+            $this->line($line);
+        }
+        
+        return $returnCode === 0;
     }
     
     private function createConfig($interfaceType, $authProvider)
